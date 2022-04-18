60a99866d62e68ed85c58c471222084a002d6827
Atharv Parmar <atharvp45@gmail.com>
2022-04-18 19:12:38 +0530

    commit for 18-04-2022

---
+++ Part_3/src/main/java/Project/UserLogin.java
@@ -0,0 +1,35 @@
+package Project;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserLogin
+ */
+@WebServlet("/UserLogin")
+public class UserLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String userLoginID = req.getParameter("user");
+		String password = req.getParameter("password");
+		
+		
+	}
+
+}
